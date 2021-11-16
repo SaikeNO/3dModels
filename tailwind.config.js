1bfd20031d9869bd60002aa55b7758ce6a71d79d
filip1139 <filip1139@gmail.com>
2021-11-16 10:16:51 +0100

    style: add basic tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
+  mode: 'jit',
   purge: {
-    content: [
-      './app/**/*.php',
-      './resources/**/*.{php,vue,js}',
-    ],
+    content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem',
+      },
+      center: true,
+    },
     extend: {
       colors: {},
     },
