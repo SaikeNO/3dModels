3f89c6c7d67749e93a9c0a09cc1d81e880306551
Mateusz Lengiewicz <mateusz.lengiewicz@prograffing.pl>
2021-12-29 15:26:32 +0100

    feat: Add ACF global fields

--- functions.php
+++ functions.php
@@ -28,57 +28,21 @@ require $composer;
 |
 */
 
+require_once __DIR__ . '/bootstrap/app.php';
+
 
 // Remove <p> and <br/> tags from Contact Form
 // add_filter('wpcf7_autop_or_not', '__return_false');
 
 
-/**
- * Disable the emoji's
- */
-function disable_emojis() {
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
-    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
-    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
-    add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
-   }
-   add_action( 'init', 'disable_emojis' );
-   
-   /**
-    * Filter function used to remove the tinymce emoji plugin.
-    * 
-    * @param array $plugins 
-    * @return array Difference betwen the two arrays
-    */
-   function disable_emojis_tinymce( $plugins ) {
-    if ( is_array( $plugins ) ) {
-    return array_diff( $plugins, array( 'wpemoji' ) );
-    } else {
-    return array();
-    }
-   }
-   
-   /**
-    * Remove emoji CDN hostname from DNS prefetching hints.
-    *
-    * @param array $urls URLs to print for resource hints.
-    * @param string $relation_type The relation type the URLs are printed for.
-    * @return array Difference betwen the two arrays.
-    */
-   function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
-    if ( 'dns-prefetch' == $relation_type ) {
-    /** This filter is documented in wp-includes/formatting.php */
-    $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
-   
-   $urls = array_diff( $urls, array( $emoji_svg_url ) );
-    }
-   
-   return $urls;
-   }
-
-require_once __DIR__ . '/bootstrap/app.php';
+// Add ACF global option for site
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Opcje strony',
+		'menu_title'	=> 'Opcje strony',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+}
